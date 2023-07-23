9491d92fcb69a5c46c4ee345a904b51bde47742a
Matthew Augier <matthew@dps.uk.com>
2023-07-23 20:59:56 +0100

    Playing

--- callbacks.php
+++ callbacks.php
@@ -1,12 +1,14 @@
 <?php
 include_once "./common/onefourone.php";
 
+$debug->log($_SERVER['SCRIPT_NAME']);
 $debug->log($argv);
 
 $script = $argv[0];
 $command = $argv[1];
 
-if($command == "--list") {
-			echo "type,Sequence,Media,title";
-			exit(0);
+if ($command == "--list") {
+	echo "Media,Playlist";
+	exit(0);
 }
+
