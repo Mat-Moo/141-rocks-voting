365203559a68365770b1d0197c52fbabec92fea5
Matthew Augier <matthew@dps.uk.com>
2023-07-23 20:39:13 +0100

    Playtime

--- common/onefourone.php
+++ common/onefourone.php
@@ -1,9 +1,9 @@
 <?php
 // get fpp base info
 include_once "/opt/fpp/www/common.php";
+$logFile = $settings['logDirectory']."/141-rocks.log";
 
-$logFile = $settings['logDirectory']."/".$pluginName.".log";
-
-include_once "./class.debug.php";
+// get debug class sorted
+include_once "/common/class.debug.php";
 $debug = new debug($logfile);
 $debug->log("FPP Matrix tools plugin is not installed, cannot use this plugin with out it");
